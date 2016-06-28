5e09e8db577e61b88332bd32d600463ea7c33695
Mark Heiges <mheiges@uga.edu>
2016-06-28 10:08:39 -0400

    install some utilities to aid troubleshooting

--- puppet/src/profiles/manifests/globus_connect_server.pp
+++ puppet/src/profiles/manifests/globus_connect_server.pp
@@ -3,4 +3,10 @@ class profiles::globus_connect_server {
   include ::epel
   include ::ebrc_yum_repo
   include ::globus_connect_server
+
+  # for troubleshooting
+  package { ['lsof', 'mlocate', 'nmap']:
+    ensure => 'installed',
+  }
+
 }
